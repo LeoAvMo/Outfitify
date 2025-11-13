534e058f07d3f665aa15d0328b125f728fc1122f
LeoAvMo <avilamolinaleonardo@gmail.com>
2025-11-13 16:03:39 +0100

    PhotosPicker implemented
    Can display correctly the images an outfir

--- Outfitify/Screens/StylingView/AddOutfitView.swift
+++ Outfitify/Screens/StylingView/AddOutfitView.swift
@@ -6,30 +6,94 @@
 //
 
 import SwiftUI
+import PhotosUI
+import CoreTransferable
 
 struct AddOutfitView: View {
     @Environment(\.dismiss) private var dismiss
     @State public var clothingType: ClothingType
+    
+    @State private var imageSelection: PhotosPickerItem?
+    @State private var image: Image?
+    @State private var selectedImageData: Data?
+    
+    
     var body: some View {
-        NavigationStack{
+        NavigationStack {
             Group {
-                Button("Take photo") {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 30)
+                        .stroke(.accent, lineWidth: 5)
+                        .fill(.background)
                     
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                        
                 }
-                .buttonStyle(.glassProminent)
+                .frame(width: 300, height: 300)
+                .task(id: imageSelection) {
+                    image = try? await imageSelection?
+                        .loadTransferable(type: Image.self)
+                }
                 
-                Button("Choose image from Gallery") {
+                VStack {
+                    PhotosPicker(selection: $imageSelection, matching: .images, photoLibrary: .shared()) {
+                        HStack {
+                            Image(systemName: "photo")
+                            Text("Choose image from Gallery")
+                        }
+                        .font(.title2)
+                        .padding(7)
+                        .frame(maxWidth: .infinity)
+                        .bold()
+                    }
+                    .buttonStyle(.glassProminent)
                     
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Image(systemName: "camera.fill")
+                            Text("Take a Photo")
+                        }
+                        .font(.title2)
+                        .padding(7)
+                        .frame(maxWidth: .infinity)
+                        .bold()
+                    }
+                    
+                    .buttonStyle(.glass)
                 }
-                .buttonStyle(.glassProminent)
+                
                 
             }
+            .padding(.horizontal)
             .navigationTitle("Add Outfit")
             .navigationBarTitleDisplayMode(.inline)
         }
     }
+    
+        
 }
 
 #Preview {
     AddOutfitView(clothingType: .topwear)
 }
+
+
+
+enum ImageState {
+    case empty
+    case loading(Progress)
+    case success(Image)
+    case failure(Error)
+}
+
+enum TransferError: Error {
+    case importFailed
+}
+
+
+
+
